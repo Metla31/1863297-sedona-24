a9231c9168fc5d5463168ca52ba315de67bdff0f
Metla31 <Metlaiceblade@yandex.ru>
2022-01-30 23:33:19 +0300

    def

--- source/js/script.js
+++ source/js/script.js
@@ -1,16 +1,28 @@
 const button = document.querySelector('.page-header__toggle');
-  const menu = document.querySelector('.main-nav');
-        button.addEventListener('click',function() {
-        if (button.classList.contains('page-header__toggle--opened')) {
-            button.classList.add('page-header__toggle--closed');
-            button.classList.remove('page-header__toggle--opened');
-            menu.classList.add('main-nav--closed');
-            menu.classList.remove('main-nav--opened');
-        }
-        else {
-            button.classList.remove('page-header__toggle--closed');
-            button.classList.add('page-header__toggle--opened');
-            menu.classList.remove('main-nav--closed');
-            menu.classList.add('main-nav--opened');
-        }
-  });
+const menu = document.querySelector('.main-nav');
+
+const closeMenu = () => {
+    button.classList.add('page-header__toggle--closed');
+    button.classList.remove('page-header__toggle--opened');
+    menu.classList.add('main-nav--closed');
+    menu.classList.remove('main-nav--opened');
+}
+
+const openMenu = () => {
+    button.classList.remove('page-header__toggle--closed');
+    button.classList.add('page-header__toggle--opened');
+    menu.classList.remove('main-nav--closed');
+    menu.classList.add('main-nav--opened');
+}
+
+if (button.classList.contains('page-header__toggle--opened')) {
+    closeMenu();
+}
+
+button.addEventListener('click',function() {
+    if (button.classList.contains('page-header__toggle--opened')) {
+        closeMenu();
+    } else {
+        openMenu();
+    }
+});
